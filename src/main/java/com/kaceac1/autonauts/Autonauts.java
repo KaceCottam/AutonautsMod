ac81e803c60ffd39191f36a061ab745c01c2aa6c
Kace Cottam <kaceac1@hotmail.com>
2019-08-07 11:32:47 -0700

    Renamed Examplemod files

--- src/main/java/com/kaceac1/autonauts/Autonauts.java
+++ src/main/java/com/kaceac1/autonauts/Autonauts.java
@@ -1,18 +1,17 @@
-package com.example.examplemod;
+package com.kaceac1.autonauts;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = Autonauts.MODID, name = Autonauts.NAME, version = Autonauts.VERSION)
+public class Autonauts
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "autonauts";
+    public static final String NAME = "Autonauts";
+    public static final String VERSION = "0.1.0";
 
     private static Logger logger;
 
@@ -26,6 +25,6 @@ public class ExampleMod
     public void init(FMLInitializationEvent event)
     {
         // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        logger.info("Autonauts Mod Loaded.");
     }
 }
