8abcb4da6275b01247a9d88255e1eca4391807e1
Kace Cottam <kaceac1@hotmail.com>
2019-08-07 11:39:58 -0700

    Added styles and assets folders

--- src/main/java/com/kaceac1/autonauts/Autonauts.java
+++ src/main/java/com/kaceac1/autonauts/Autonauts.java
@@ -7,24 +7,21 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Autonauts.MODID, name = Autonauts.NAME, version = Autonauts.VERSION)
-public class Autonauts
-{
-    public static final String MODID = "autonauts";
-    public static final String NAME = "Autonauts";
-    public static final String VERSION = "0.1.0";
+public class Autonauts {
+  public static final String MODID = "autonauts";
+  public static final String NAME = "Autonauts";
+  public static final String VERSION = "0.1.0";
 
-    private static Logger logger;
+  private static Logger logger;
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        logger = event.getModLog();
-    }
+  @EventHandler
+  public void preInit(FMLPreInitializationEvent event) {
+    logger = event.getModLog();
+  }
 
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        logger.info("Autonauts Mod Loaded.");
-    }
+  @EventHandler
+  public void init(FMLInitializationEvent event) {
+    // some example code
+    logger.info("Autonauts Mod Loaded.");
+  }
 }
